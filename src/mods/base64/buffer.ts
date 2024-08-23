265a41845ac5160cf976bbb0325c4e3d2bb5513d
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-08-23 22:23:46 +0700

    d

--- src/mods/base64/buffer.ts
+++ src/mods/base64/buffer.ts
@@ -1,11 +1,9 @@
-import { BytesOrCopiable, Copied } from "@hazae41/box"
-import { Result } from "@hazae41/result"
 import { Buffers } from "libs/buffers/buffers.js"
 import { Bytes } from "libs/bytes/bytes.js"
+import { BytesOrCopiable, Copied } from "libs/copiable/index.js"
 import { Adapter } from "./adapter.js"
-import { DecodeError, EncodeError } from "./errors.js"
 
-export function fromBuffer(): Adapter {
+export function fromBuffer() {
 
   function getBytes(bytes: BytesOrCopiable) {
     return "bytes" in bytes ? bytes.bytes : bytes
@@ -15,41 +13,17 @@ export function fromBuffer(): Adapter {
     return Buffers.fromView(getBytes(bytes)).toString("base64")
   }
 
-  function tryEncodePadded(bytes: BytesOrCopiable) {
-    return Result.runAndWrapSync(() => {
-      return encodePaddedOrThrow(bytes)
-    }).mapErrSync(EncodeError.from)
-  }
-
   function decodePaddedOrThrow(text: string) {
     return new Copied(Bytes.fromView(Buffer.from(text, "base64")))
-  }
-
-  function tryDecodePadded(text: string) {
-    return Result.runAndWrapSync(() => {
-      return decodePaddedOrThrow(text)
-    }).mapErrSync(DecodeError.from)
   }
 
   function encodeUnpaddedOrThrow(bytes: BytesOrCopiable) {
     return Buffers.fromView(getBytes(bytes)).toString("base64").replaceAll("=", "")
   }
 
-  function tryEncodeUnpadded(bytes: BytesOrCopiable) {
-    return Result.runAndWrapSync(() => {
-      return encodeUnpaddedOrThrow(bytes)
-    }).mapErrSync(EncodeError.from)
-  }
-
   function decodeUnpaddedOrThrow(text: string) {
     return new Copied(Bytes.fromView(Buffer.from(text, "base64")))
   }
 
-  function tryDecodeUnpadded(text: string) {
-    return Result.runAndWrapSync(() => {
-      return decodeUnpaddedOrThrow(text)
-    }).mapErrSync(DecodeError.from)
-  }
-
-  return { encodePaddedOrThrow, tryEncodePadded, decodePaddedOrThrow, tryDecodePadded, encodeUnpaddedOrThrow, tryEncodeUnpadded, decodeUnpaddedOrThrow, tryDecodeUnpadded }
+  return { encodePaddedOrThrow, decodePaddedOrThrow, encodeUnpaddedOrThrow, decodeUnpaddedOrThrow } satisfies Adapter
 }
