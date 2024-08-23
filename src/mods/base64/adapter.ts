001c300d2e4ba7e1d53148b90f53ff268f4bba3f
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-08-23 22:21:03 +0700

    fix

--- src/mods/base64/adapter.ts
+++ src/mods/base64/adapter.ts
@@ -1,33 +1,22 @@
-import { BytesOrCopiable, Copiable } from "@hazae41/box"
-import { Nullable } from "@hazae41/option"
-import { Result } from "@hazae41/result"
+import { Nullable, Option, Some } from "@hazae41/option"
+import { BytesOrCopiable, Copiable } from "libs/copiable/index.js"
 import { fromBuffer } from "./buffer.js"
-import { DecodeError, EncodeError } from "./errors.js"
 
-let global: Nullable<Adapter> = fromBuffer()
+let global: Option<Adapter> = new Some(fromBuffer())
 
 export function get() {
-  if (global == null)
-    throw new Error("No Base64 adapter found")
   return global
 }
 
-export function set(value?: Nullable<Adapter>) {
-  global = value
+export function set(value: Nullable<Adapter>) {
+  global = Option.wrap(value)
 }
-
 
 export interface Adapter {
   encodePaddedOrThrow(bytes: BytesOrCopiable): string
-  tryEncodePadded(bytes: BytesOrCopiable): Result<string, EncodeError>
-
   decodePaddedOrThrow(text: string): Copiable
-  tryDecodePadded(text: string): Result<Copiable, DecodeError>
 
   encodeUnpaddedOrThrow(bytes: BytesOrCopiable): string
-  tryEncodeUnpadded(bytes: BytesOrCopiable): Result<string, EncodeError>
-
   decodeUnpaddedOrThrow(text: string): Copiable
-  tryDecodeUnpadded(text: string): Result<Copiable, DecodeError>
 }
 
